85745fa4278ba4f6641afaaca99bb8a8bb8a6ba8
npalomares <saucealito26@yahoo.com>
2016-08-22 21:26:32 -0700

    PHP starter file for webd_166

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>PHP Starter</title>
+	<!-- <link rel="stylesheet" href=""> -->
+	
+	<!-- jQuery -->
+	<!--<script src=""></script>-->
+	<!-- Custom JS -->
+	<!--<script src=""></script>-->	
+
+</head>
+<body class="page">
+
+<?php echo "<h1>" . "PHP Starter File!" . "</h1>"; ?>
+
+<?php 
+
+/*
+	Filename: index.php
+	Book Reference: Script - 
+	Created By: Nicholas Palomares
+*/
+
+
+?>
+
+
+</body>
+</html>
