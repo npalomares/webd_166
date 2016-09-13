8739557fee3c0e9324440903388fdbdcb144fca5
npalomares <saucealito26@yahoo.com>
2016-09-12 20:19:19 -0700

    assignment two has been added

---
+++ AssignmentTwo/variables.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NPalomares - Variables</title>
+</head>
+<body class="variables">
+	<?php
+	/* Don't forget to answer the zip question		
+		Script 2.3 - variables.php
+		Nicholas Palomares
+	*/
+
+	// Ficticious address
+	$street = "123 Fake Street";
+	$city = "San Diego";
+	$state = "California";
+	$zip = "92120"; // use zip as a string
+
+	//Let's display the address in the browser
+	print "
+			<h2>My Address is:</h2>
+			<address>
+				<p>
+					$street<br>
+					$city, $state $zip
+				</p>	
+			</address>";
+	?>
+
+
+
+
+
+
+</body>
+</html>
