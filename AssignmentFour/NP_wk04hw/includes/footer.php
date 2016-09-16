06765f26177619536eda73a43409b25e3b7bd423
Nicholas <nicholaspalomares@gmail.com>
2016-09-15 19:33:12 -0700

    Updated and submitted assignment four

---
+++ AssignmentFour/NP_wk04hw/includes/footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+    <div class="container">
+        <p class="text-center"><?php echo Date('Y'); ?> Powered by PHP</p>
+    </div>
+</footer>
+
+</body>
+</html>
--- AssignmentFour/index.php
+++ AssignmentFour/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>WEBD_166 | Feedback Form</title>
+	<title>WEBD_166 | Product Cost Calculator</title>
 	<link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
@@ -14,42 +14,45 @@
 			<div class="form-wrapper">
 				<?php
 					/*
-					* Filename: NP_wk03hw.php
-					* Book reference: Script 3.1
+					* Filename: NP_wk04hw.php
+					* Book reference: Script 4.1
 					* Created by: Nicholas Palomares
 					*/
 		            
-		            print '<h3 class="text-center">Please complete the form below</h3>';
+		            print '<h3 class="text-center">Fill out this form to find out the total cost</h3>';
 		            
 				?>					
-				<form method="post" action="form_handle.php">
+				<form method="post" action="handle_calc.php">
 					<div class="form-component">
-						<select name="title">
-							<option value="Mr">Mr.</option>
-							<option value="Mrs">Mrs.</option>
-							<option value="Ms">Ms.</option>
+						<label>* Price: </label>
+						<input type="text" class="price" name="price" placeholder="$" required>						
+					</div>
+					<div class="form-component">
+						<label>* Quanity: </label>
+						<input type="text" class="quantity" name="quantity" required>
+					</div>
+					<div class="form-component">
+						<label>* Discount: </label>
+						<input type="text" class="discount" name="discount" placeholder="$" required>
+					</div>
+					<div class="form-component">
+						<label>* Tax: (%)</label>
+						<input type="text" class="tax" name="tax" required>
+					</div>
+					<div class="form-component">
+						<label>* Shipping Method:</label>
+						<select name="shipping" required>
+							<option value="20.00">Like Today - $20</option>
+							<option value="10.00">Tomorrow - $10</option>
+							<option value="5.00">Someday Soon - $5</option>
 						</select>
 					</div>
 					<div class="form-component">
-						<label>Full Name: </label>
-						<input type="text" class="fname" name="fname" required>
-					</div>
+						<label>* Number of Payments to Make:</label>
+						<input type="text" class="payments" name="payments" size="5" required>
+					</div>							
 					<div class="form-component">
-						<label>Email: </label>
-						<input type="email" class="email" name="email" required>
-					</div>
-					<div class="form-component">
-						<label>Response:<br> This is... </label>
-						<span class="rad-wrap"><input type="radio" name="response" value="Awesome" required>Awesome</span>
-						<span class="rad-wrap"><input type="radio" name="response" value="Excellent">Excellent</span>
-						<span class="rad-wrap"><input type="radio" name="response" value="Phenomenal">Phenomenal</span>
-					</div>
-					<div class="form-component">
-						<label>Comments: </label>
-						<textarea name="comments" rows="3" cols="30" class="comments" value="Enter your Feedback" required></textarea>
-					</div>		
-					<div class="form-component">
-						<input type="submit" name="submit" class="btn btn-primary" value="Submit">
+						<input type="submit" name="submit" class="btn btn-primary" value="Calculate">
 					</div>																
 				</form>
 			</div>
